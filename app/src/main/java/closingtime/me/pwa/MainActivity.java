aedbc0ef84690eccc06fd7c030f5587666d8d4f5
Alexander Davis <alexanderdavis.net@googlemail.com>
2019-03-26 20:16:58 +0000

    first commit

---
+++ app/src/main/java/closingtime/me/pwa/MainActivity.java
@@ -0,0 +1,157 @@
+package closingtime.me.pwa;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.content.res.AssetManager;
+import android.os.Bundle;
+import android.webkit.*;
+
+import org.json.*;
+
+import java.io.*;
+import java.util.Arrays;
+
+import androidx.appcompat.app.AppCompatActivity;
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final int REQUEST_LOCATION_PERMISSION = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        loadManifest();
+        setDisplay(this);
+        setOrientation(this);
+        setName(this);
+        setContentView(R.layout.activity_main);
+        setWebView((WebView) this.findViewById(R.id.webView));
+        requestLocationPermission();
+
+    }
+
+    private void setDisplay(Activity activity) {
+        if (this.manifestObject.optString("display").equals("fullscreen")) {
+            activity.setTheme(R.style.FullscreenTheme);
+        } else {
+            activity.setTheme(R.style.AppTheme);
+        }
+    }
+
+    private void setName(Activity activity) {
+        String name = this.manifestObject.optString("name");
+        if (!name.isEmpty()) {
+            activity.setTitle(name);
+        }
+    }
+
+    private static final String ANY = "any";
+    private static final String NATURAL = "natural";
+    private static final String PORTRAIT_PRIMARY = "portrait-primary";
+    private static final String PORTRAIT_SECONDARY = "portrait-secondary";
+    private static final String LANDSCAPE_PRIMARY = "landscape-primary";
+    private static final String LANDSCAPE_SECONDARY = "landscape-secondary";
+    private static final String PORTRAIT = "portrait";
+    private static final String LANDSCAPE = "landscape";
+
+    private void setOrientation(Activity activity) {
+        String orientation = this.manifestObject.optString("orientation");
+        switch (orientation) {
+            case LANDSCAPE_PRIMARY:
+                activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                break;
+            case PORTRAIT_PRIMARY:
+                activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                break;
+            case LANDSCAPE:
+                activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
+                break;
+            case PORTRAIT:
+                activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT);
+                break;
+            case LANDSCAPE_SECONDARY:
+                activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE);
+                break;
+            case PORTRAIT_SECONDARY:
+                activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT);
+                break;
+            case ANY:
+            case NATURAL:
+            default:
+                activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+                break;
+        }
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private void setWebView(WebView myWebView) {
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setGeolocationEnabled(true);
+        String start_url = this.manifestObject.optString("start_url");
+        String scope = this.manifestObject.optString("scope");
+        myWebView.setWebViewClient(new PwaWebViewClient(start_url, scope));
+        myWebView.setWebChromeClient(new WebChromeClient() {
+            public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+                callback.invoke(origin, true, false);
+            }
+        });
+        myWebView.loadUrl(start_url);
+    }
+
+    private static final String DEFAULT_MANIFEST_FILE = "manifest.json";
+    private JSONObject manifestObject;
+
+    private void loadManifest() {
+        if (this.assetExists((DEFAULT_MANIFEST_FILE))) {
+            try {
+                this.manifestObject = this.loadLocalManifest();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private boolean assetExists(String asset) {
+        final AssetManager assetManager = this.getResources().getAssets();
+        try {
+            return Arrays.asList(assetManager.list("")).contains(asset);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    private JSONObject loadLocalManifest() throws JSONException {
+        try {
+            InputStream inputStream = this.getResources().getAssets().open(DEFAULT_MANIFEST_FILE);
+            int size = inputStream.available();
+            byte[] bytes = new byte[size];
+            int readBytes = inputStream.read(bytes);
+            inputStream.close();
+            if (readBytes > 0) {
+                String jsonString = new String(bytes, "UTF-8");
+                return new JSONObject(jsonString);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @AfterPermissionGranted(REQUEST_LOCATION_PERMISSION)
+    public void requestLocationPermission() {
+        String[] perms = {Manifest.permission.ACCESS_FINE_LOCATION};
+        if(!EasyPermissions.hasPermissions(this, perms)) {
+            EasyPermissions.requestPermissions(this, "Please grant the location permission", REQUEST_LOCATION_PERMISSION, perms);
+        }
+    }
+}
